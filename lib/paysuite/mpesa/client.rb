9fb35db398b950eeca08691a9859f55e3fbd9ed3
Edson Michaque <edson@michaque.com>
2020-07-04 14:42:29 +0200

    Add client class

---
+++ lib/paysuite/mpesa/client.rb
@@ -0,0 +1,20 @@
+module Paysuite
+  module MPesa
+    class Client
+      def initialize
+      end
+
+      def send(intent)
+      end
+
+      def receive(intent)
+      end
+
+      def query(intent)
+      end
+
+      def revert(intent)
+      end
+    end
+  end
+end
