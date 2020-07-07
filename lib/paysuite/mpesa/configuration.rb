febde319135240aae7b3d81163eaa5f56ca522f1
Edson Michaque <edson@michaque.com>
2020-07-07 07:16:58 +0200

    Add accessors for origin

--- lib/paysuite/mpesa/configuration.rb
+++ lib/paysuite/mpesa/configuration.rb
@@ -1,18 +1,19 @@
 module Paysuite
   module MPesa
-    attr_accessor :access_token
-    attr_accessor :api_key
-    attr_accessor :public_key
-    attr_accessor :user_agent
-    attr_accessor :verify_ssl
-    attr_accessor :timeout
-    attr_accessor :debugging
-    attr_accessor :environment
-    attr_accessor :initiator_identifier
-    attr_accessor :security_credential
-    attr_accessor :service_provider_code
-
     class Configurarion
+      attr_accessor :access_token
+      attr_accessor :api_key
+      attr_accessor :public_key
+      attr_accessor :user_agent
+      attr_accessor :verify_ssl
+      attr_accessor :timeout
+      attr_accessor :debugging
+      attr_accessor :environment
+      attr_accessor :initiator_identifier
+      attr_accessor :security_credential
+      attr_accessor :service_provider_code
+      attr_accessor :origin
+
       def initialize
         yield(self) if block_given?
       end
